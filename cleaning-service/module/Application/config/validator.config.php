8488bd23579ffe58d125031364a4592193d451da
Irawan123 <irawan.ahak@gmail.com>
2015-02-10 23:17:37 +0700

    Pindahkan dari isi cleaning-service keluar n master
    Master Customer, Pegawai, Atm, List foto detail, Pekerjaan, Lokasi, Zona

--- cleaning-service/module/Application/config/validator.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'mappings' => array(
-        'xml' => array(
-            __DIR__ . '/validator/Application.SkeletonApp.Domain.Master.Dummy.xml',
-        ),
-    ),
-);
